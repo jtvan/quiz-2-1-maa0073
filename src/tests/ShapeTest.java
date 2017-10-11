9de07a8f7397a1c95a26e35b2e124b204f99afad
jtvan <jtv0006@stu.wvu.edu>
2017-10-11 19:04:30 -0400

    .

--- src/tests/ShapeTest.java
+++ src/tests/ShapeTest.java
@@ -4,17 +4,29 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import shapes.Circle;
+import shapes.Square;
+
 public class ShapeTest {
 
 	/*
 	 * Write a test for Shape's compareTo method.
 	 * 
-	 * Also create SquareTest.java and CircleTest.java JUnit test cases.
-	 * Test each class's constructor method and calculateArea method.
+	 * Also create SquareTest.java and CircleTest.java JUnit test cases. Test
+	 * each class's constructor method and calculateArea method.
 	 */
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		Square sqr = new Square(4);
+		Square sw1 = new Square(2);
+		assertTrue(sqr.compareTo(sqr) == 0);
+		assertTrue(sqr.compareTo(sw1) == 1);
+		assertTrue(sw1.compareTo(sqr) == -1);
+		
+		Circle c1 = new Circle(4);
+		Circle c2 = new Circle(2);
+		assertTrue(c1.compareTo(c1) == 0);
+		assertTrue(c1.compareTo(c2) == 1);
+		assertTrue(c2.compareTo(c1) == -1);
 	}
-
 }
